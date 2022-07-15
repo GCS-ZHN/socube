0f16493e622a5c5831d0e9ba4cc0d995706111e8
GCS-ZHN <zhang.h.n@foxmail.com>
2022-07-15 16:46:29 +0800

    release visualization code by jvision

---
+++ Java/socube_vision/src/test/java/top/gcszhn/test/PlotTest.java
@@ -0,0 +1,116 @@
+package top.gcszhn.test;
+import top.gcszhn.jvision.RingDiagram;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class PlotTest {
+    @Test
+    public void test() throws IOException {
+        String configStr;
+        try (FileInputStream input = new FileInputStream("data/config.json")) {
+            configStr = new String(input.readAllBytes());
+        }
+        int width = 600, height = 600;
+        int df = 15;
+        float[] radioRange = new float[]{50, 200};
+        int startAngle = 90, arcAngle = -(360 - 360 / df);
+        float gapRatio = 0.2f;
+        int step = df - 1;
+        boolean withTitle = false;
+        boolean balance = true;
+        JSONObject config = JSONObject.parseObject(configStr);
+        String format = "pdf";
+        for (String type: config.getJSONObject("precision").keySet()) {
+            try {
+                JSONObject subConfig = config.getJSONObject("precision").getJSONObject(type);
+                float[] valueRange = new float[]{
+                    subConfig.getFloatValue("min"), 
+                    subConfig.getFloatValue("max")
+                };
+                RingDiagram ringDiagram = new RingDiagram(
+                    withTitle ? "Precision": null, 
+                    width, 
+                    height, 
+                    gapRatio, 
+                    radioRange, 
+                    valueRange, 
+                    startAngle, 
+                    arcAngle, 
+                    (valueRange[1]-valueRange[0]) / step,
+                    balance);
+                ringDiagram.loadData(String.format("data/data-precision-%s.csv", type));
+                ringDiagram.draw(String.format("data/data-precision-%s.%s", type, format));
+            } catch (RuntimeException e) {
+                if (e.getCause() instanceof FileNotFoundException) {
+                    System.out.println(e.getCause().getMessage());
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        for (String type: config.getJSONObject("recall").keySet()) {
+            try{
+                JSONObject subConfig = config.getJSONObject("recall").getJSONObject(type);
+                float[] valueRange = new float[]{
+                    subConfig.getFloatValue("min"), 
+                    subConfig.getFloatValue("max")
+                };
+                RingDiagram ringDiagram = new RingDiagram(
+                    withTitle ? "Recall": null, 
+                    width, 
+                    height, 
+                    gapRatio, 
+                    radioRange, 
+                    valueRange, 
+                    startAngle, 
+                    arcAngle, 
+                    (valueRange[1]-valueRange[0]) / step,
+                    balance);
+                ringDiagram.loadData(String.format("data/data-recall-%s.csv", type));
+                ringDiagram.draw(String.format("data/data-recall-%s.%s", type, format));
+            } catch (RuntimeException e) {
+                if (e.getCause() instanceof FileNotFoundException) {
+                    System.out.println(e.getCause().getMessage());
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        for (String type: config.getJSONObject("TNR").keySet()) {
+            try {
+                JSONObject subConfig = config.getJSONObject("TNR").getJSONObject(type);
+                float[] valueRange = new float[]{
+                    subConfig.getFloatValue("min"), 
+                    subConfig.getFloatValue("max")
+                };
+                RingDiagram ringDiagram = new RingDiagram(
+                    withTitle ? "TNR": null, 
+                    width, 
+                    height, 
+                    gapRatio, 
+                    radioRange, 
+                    valueRange, 
+                    startAngle, 
+                    arcAngle, 
+                    (valueRange[1]-valueRange[0]) / step,
+                    balance);
+                ringDiagram.loadData(String.format("data/data-TNR-%s.csv", type));
+                ringDiagram.draw(String.format("data/data-TNR-%s.%s", type, format));
+            } catch (RuntimeException e) {
+                if (e.getCause() instanceof FileNotFoundException) {
+                    System.out.println(e.getCause().getMessage());
+                } else {
+                    throw e;
+                }
+            }
+        }
+    }
+}
