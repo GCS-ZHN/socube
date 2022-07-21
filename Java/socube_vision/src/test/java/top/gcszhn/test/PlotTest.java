ad2cca4ad559970946318998760da9d472950cc6
GCS-ZHN <zhang.h.n@foxmail.com>
2022-07-21 21:58:45 +0800

    update to jvision v1.2 test

--- Java/socube_vision/src/test/java/top/gcszhn/test/PlotTest.java
+++ Java/socube_vision/src/test/java/top/gcszhn/test/PlotTest.java
@@ -1,5 +1,6 @@
 package top.gcszhn.test;
-import top.gcszhn.jvision.RingDiagram;
+import top.gcszhn.jvision.JvisionException;
+import top.gcszhn.jvision.chart.RingDiagram;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,12 +12,13 @@ import com.alibaba.fastjson.JSONObject;
 
 public class PlotTest {
     @Test
-    public void test() throws IOException {
+    public void test() throws IOException, JvisionException {
         String configStr;
         try (FileInputStream input = new FileInputStream("data/config.json")) {
             configStr = new String(input.readAllBytes());
         }
         int width = 600, height = 600;
+        String fontFamily = "Calibri";
         int df = 15;
         float[] radioRange = new float[]{50, 200};
         int startAngle = 90, arcAngle = -(360 - 360 / df);
@@ -44,9 +46,10 @@ public class PlotTest {
                     arcAngle, 
                     (valueRange[1]-valueRange[0]) / step,
                     balance);
+                ringDiagram.setFontFamily(fontFamily);
                 ringDiagram.loadData(String.format("data/data-precision-%s.csv", type));
                 ringDiagram.draw(String.format("data/data-precision-%s.%s", type, format));
-            } catch (RuntimeException e) {
+            } catch (JvisionException e) {
                 if (e.getCause() instanceof FileNotFoundException) {
                     System.out.println(e.getCause().getMessage());
                 } else {
@@ -73,9 +76,10 @@ public class PlotTest {
                     arcAngle, 
                     (valueRange[1]-valueRange[0]) / step,
                     balance);
+                ringDiagram.setFontFamily(fontFamily);
                 ringDiagram.loadData(String.format("data/data-recall-%s.csv", type));
                 ringDiagram.draw(String.format("data/data-recall-%s.%s", type, format));
-            } catch (RuntimeException e) {
+            } catch (JvisionException e) {
                 if (e.getCause() instanceof FileNotFoundException) {
                     System.out.println(e.getCause().getMessage());
                 } else {
@@ -102,9 +106,10 @@ public class PlotTest {
                     arcAngle, 
                     (valueRange[1]-valueRange[0]) / step,
                     balance);
+                ringDiagram.setFontFamily(fontFamily);
                 ringDiagram.loadData(String.format("data/data-TNR-%s.csv", type));
                 ringDiagram.draw(String.format("data/data-TNR-%s.%s", type, format));
-            } catch (RuntimeException e) {
+            } catch (JvisionException e) {
                 if (e.getCause() instanceof FileNotFoundException) {
                     System.out.println(e.getCause().getMessage());
                 } else {
