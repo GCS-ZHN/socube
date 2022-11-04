f64a7dc6455edfc26834281de976c4c46e25f531
GCS-ZHN <zhang.h.n@foxmail.com>
2022-11-04 22:10:25 +0800

    release v1.1

--- src/socube/__init__.py
+++ src/socube/__init__.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__ = "1.1rc2"
+__version__ = "1.1"
 __author__ = "Zhang.H.N"
 __email__ = "zhang.h.n@foxmail.com"
 __url__ = "https://github/GCS-ZHN/socube/"
